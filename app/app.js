754616606c14bd80fc98ce5d3c5799eff023bd4f
Messidt <piotrekksz@gmail.com>
2017-10-17 14:09:13 +0200

    routing
    Created routing paths and views.

--- app/app.js
+++ app/app.js
@@ -1,6 +1,22 @@
-var moviesModule = angular.module('moviesModule', []);
+var moviesModule = angular.module('moviesModule', ['ngRoute']);
+
+moviesModule.config(['$routeProvider', function($routeProvider){
+  $routeProvider
+  .when('/home', {
+    templateUrl: 'views/home.html'
+  })
+  .when('/list', {
+    templateUrl: 'views/list.html',
+    controller: 'MoviesController'
+  })
+  .otherwise({
+    redirectTo: '/home'
+  })
+}]);
+
 
 moviesModule.controller('MoviesController', ['$scope', '$http', function($scope, $http){
+
 
   $http.get('content/data/movies.json').then(function(response){
     $scope.movies = response.data;
